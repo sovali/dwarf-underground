5c548214cdf9e85f07723ca65146d27751e51459
Selin Ovali <sovali@purdue.edu>
2017-07-06 16:10:28 -0400

    Added sponsor box but is out of line

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Banner from './Banner'
 import Header from './Header'
 import Content from './Content'
+import Sponsor from './Sponsor'
 import Bar from './Bar'
 import logo from './logo.svg';
 import './App.css';
@@ -9,11 +10,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <div class="expanded row header">
       <div className="Container">
         <Banner/>
         <Header/>
         <Content/>
         <Bar/>
+        <Sponsor/>
+      </div>
       </div>
     );
   }
